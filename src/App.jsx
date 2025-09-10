ebec1c4d0b59a1caef36b10a35172f08b78e1b24
Peter Teoh <htmldeveloper@gmail.com>
2025-09-10 12:06:40 +0800

    new file:   Dockerfile 	new file:   eslint.config.js 	new file:   index.html 	new file:   myrun.sh 	new file:   nginx.conf 	new file:   package-lock.json 	new file:   package.json 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/components/MyName.jsx 	new file:   src/components/ProductPage.jsx 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import ProductPage from './components/ProductPage';
+import MyName from './components/MyName';
+
+function App() {
+  return (
+    <>
+      <MyName />
+      <ProductPage />
+    </>
+  )
+}
+
+export default App;
